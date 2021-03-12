438d7f132cca958569075987fd2ccf2586126e8f
Ashokbabu Miriyala <ashokbabu.miriyala@gmail.com>
2021-03-12 14:51:03 +0530

    included services

--- MyStoreUI/src/app/Shared/login/login.page.ts
+++ MyStoreUI/src/app/Shared/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RegistrationServiceService} from '../../Shared/registration-service.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private registrationServiceService:RegistrationServiceService) { }
 
   ngOnInit() {
+  }
+  async validateUser(): Promise<void> {
+    const dataObject = { ProviderUserName:'Ashok',Password:'8106939983'  };
+    this.registrationServiceService.validateUser('ProviderLogin', dataObject)
+      .subscribe((data: any) => {
+       
+      },
+        (error: any) => {
+          if (error.originalError.status === 401) {
+          
+          }         
+        });
   }
 
 }
