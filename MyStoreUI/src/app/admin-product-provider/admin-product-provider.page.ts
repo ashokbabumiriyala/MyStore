70d1b6edc1761e38866ed44dbfbd50bfca73110e
kata419 <satish.k419@gmail.com>
2021-04-08 20:55:29 +0530

    search and sort

--- MyStoreUI/src/app/admin-product-provider/admin-product-provider.page.ts
+++ MyStoreUI/src/app/admin-product-provider/admin-product-provider.page.ts
@@ -7,17 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductProviderPage implements OnInit {
   public items: any = [];
+  public masterData:any = [];
+  public searchTerm: string = "";
+  public asc:boolean = true;
 
   constructor() {
     this.items = [
-      {master:'Master-1', expanded: true },
-      {master:'Master-2', expanded: false },
-      {master:'Master-3', expanded: false },
-      {master:'Master-4', expanded: false },
-      {master:'Master-5', expanded: false }
+      {master:'A', expanded: true },
+      {master:'B', expanded: false },
+      {master:'C', expanded: false },
+      {master:'D', expanded: false },
+      {master:'E', expanded: false }
     ];
+    Object.assign(this.masterData,this.items);
   }
   ngOnInit() {
+  }
+  sorting() {
+    this.asc = !this.asc;
+     this.items.sort(function (a, b) {
+      if(a.master < b.master) {
+        return 1;
+    } else {
+        return -1; 
+    }
+      return 0;
+    });
+    console.log(this.items);
+  }
+  filterItems() {
+    this.masterData = this.items.filter(item => {
+      return item.master.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+    });
   }
   expandItem(item): void {
     if (item.expanded) {
--- MyStoreUI/src/app/row-expand-accordian/row-expand-accordian.component.ts
+++ MyStoreUI/src/app/row-expand-accordian/row-expand-accordian.component.ts
@@ -6,11 +6,20 @@ import { Component, AfterViewInit, Input, ViewChild,  ElementRef, Renderer2 } fr
   styleUrls: ['./row-expand-accordian.component.scss'],
 })
 export class RowExpandAccordianComponent implements AfterViewInit {
+  public items: any = [];
   @ViewChild("expandWrapper", { read: ElementRef }) expandWrapper: ElementRef;
   @Input("expanded") expanded: boolean;
   @Input("expandHeight") expandHeight: string = "120px";
 
-  constructor(public renderer: Renderer2) {}
+  constructor(public renderer: Renderer2) {
+    this.items = [
+      {store:'Store-1', status:'Deactive'},
+      {store:'Store-2', status:'Deactive'},
+      {store:'Store-3', status:'Deactive'},
+      {store:'Store-4', status:'Active'},
+      {store:'Store-5', status:'Active'}
+    ];
+  }
 
   ngAfterViewInit() {
     this.renderer.setStyle(this.expandWrapper.nativeElement, "height", this.expandHeight);
