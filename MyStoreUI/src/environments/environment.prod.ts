dc2d69c2c6e163c5381744975b3bcf5a4cb64ddc
Ashokbabu Miriyala <ashokbabu.miriyala@gmail.com>
2021-05-13 23:43:55 +0530

    test  env changes

--- MyStoreUI/src/environments/environment.prod.ts
+++ MyStoreUI/src/environments/environment.prod.ts
@@ -1,7 +1,17 @@
 export const environment = {
+
+
   production: true, 
-  authenticationServiceUrl: 'https://my3api.itprototypes.com/api/Authentication/',
-  storeMasterServiceUrl: 'https://my3api.itprototypes.com/api/Store/',
-  serviceMasterServiceUrl: 'https://my3api.itprototypes.com/api/Service/',
-  adminServiceUrl: 'https://my3api.itprototypes.com/api/Admin/'
+  //live 
+  // authenticationServiceUrl: 'https://my3api.itprototypes.com/api/Authentication/',
+  // storeMasterServiceUrl: 'https://my3api.itprototypes.com/api/Store/',
+  // serviceMasterServiceUrl: 'https://my3api.itprototypes.com/api/Service/',
+  // adminServiceUrl: 'https://my3api.itprototypes.com/api/Admin/'
+
+
+//testing
+  authenticationServiceUrl: 'https://my3apitest.itprototypes.com/api/Authentication/',
+  storeMasterServiceUrl: 'https://my3apitest.itprototypes.com/api/Store/',
+  serviceMasterServiceUrl: 'https://my3apitest.itprototypes.com/api/Service/',
+  adminServiceUrl: 'https://my3apitest.itprototypes.com/api/Admin/'
 };
